59b53863d11ff24ff9bb91075284b74963b80772
Pranav Chugh <40795912+TheMediocreCoder@users.noreply.github.com>
2022-06-19 16:11:00 -0700

    Push / Update Passwords from CyberArk to Azure Key Vault

---
+++ Update-AzKV.ps1
@@ -0,0 +1,52 @@
+﻿[CmdletBinding()]
+param(
+    [parameter(Mandatory = $true)]
+	[ValidateNotNullOrEmpty()]
+	[string]$KeyVaultName,
+
+    [parameter(Mandatory = $true)]
+	[ValidateNotNullOrEmpty()]
+	[string]$Secret_Name,
+
+    [parameter(Mandatory = $true)]
+	[ValidateNotNullOrEmpty()]
+	[String]$Secret,
+
+    [parameter(Mandatory = $true)]
+	[ValidateNotNullOrEmpty()]
+	[String]$logon_username,
+
+    [parameter(Mandatory = $true)]
+	[ValidateNotNullOrEmpty()]
+	[String]$logon_password
+)
+
+$ErrorActionPreference = "Stop"
+$Error.Clear()
+
+#Connect to Azure using Logon Account Creds
+try
+{
+    $creds = New-Object -typename System.Management.Automation.PSCredential($logon_username, ($logon_password | ConvertTo-SecureString -AsPlainText -Force))
+    Connect-AzAccount -Credential $creds | Out-Null
+    Write-Output "Successfully Connected to Azure using $logon_username"
+}
+catch
+{
+    Write-Output "Failed to authenticate to Azure. Could be Network Issue or Crednetial issue.`n$($Error[0].Exception)"
+}
+
+#Convert the Master Account Password to SecureString
+$SecureSecret = $Secret | ConvertTo-SecureString -Force -AsPlainText
+
+
+#Push/Update the Secret to the Azure KeyVault
+try
+{
+    Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name $Secret_Name -SecretValue $SecureSecret | Out-Null
+    Write-Output "Pushed the secret successfully"
+}
+catch
+{
+    Write-Output "Failed to update the Secret.`n $($Error[0].Exception)"
+}
